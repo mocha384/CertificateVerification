bb479c5e6abb09f9389ab75d491583a005359c0b
lightswarm124 <lightswarm124@github.com>
2016-08-13 23:04:08 -0400

    1st update

--- CertificateVerfication.sol
+++ CertificateVerfication.sol
@@ -1,19 +1,25 @@
-struct University {
-  string name,
-  bool verified
-}
-uint university_id = 0;
-mapping (address => bool) universityMap ;
-mapping (uint => University) university ;
-
-function addUniversity(_name) {
-  if(universityMap[msg.sender] == true) {
-    return;
+contract certVeri {
+  struct University {
+    string name,
+    bool verified
   }
-  else {
-    uint uid = ++university_id;
-    University univ = university[uid];
-    univ.name = _name;
-    univ.verified = false;
+
+  uint university_id = 0;
+  mapping (address => bool) universityMap ;
+  mapping (uint => University) university ;
+
+  function addUniversity (_name) {
+    if(universityMap[msg.sender] == true) {
+      return;
+    } else {
+      uint uid = ++university_id;
+      University univ = university[uid];
+      univ.name = _name;
+      univ.verified = false;
+    }
+  }
+
+  function addCerti () {
+    throw;
   }
 }
